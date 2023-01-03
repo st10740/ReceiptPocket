78291e731fa0f87bf7b2cd947493ededc3064cbf
st10740 <ivywang2015@gmail.com>
2023-01-03 14:49:08 +0800

    register UI

--- app/src/main/java/com/example/receiptpocket/MainActivity.kt
+++ app/src/main/java/com/example/receiptpocket/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.receiptpocket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+    private lateinit var registerBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // set ToolBar
-        val toolbar = findViewById<Toolbar>(R.id.login_toolbar)
-        toolbar.title = ""
-        setSupportActionBar(toolbar)
+        // Set ToolBar
+//        toolbar = findViewById(R.id.login_toolbar)
+//        toolbar.title = ""
+//        setSupportActionBar(toolbar)
+
+        // Register button
+        registerBtn = findViewById(R.id.register_btn)
+        registerBtn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/receiptpocket/RegisterActivity.kt
+++ app/src/main/java/com/example/receiptpocket/RegisterActivity.kt
@@ -2,10 +2,18 @@ package com.example.receiptpocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var backBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Back Button
+        backBtn = findViewById(R.id.back_btn)
+        backBtn.setOnClickListener { finish() }
     }
 }
