f844fda74cf3b3f1b85dfa6efe97ed281598fdbf
st10740 <ivywang2015@gmail.com>
2023-01-03 13:48:35 +0800

    login UI

--- app/src/main/java/com/example/receiptpocket/MainActivity.kt
+++ app/src/main/java/com/example/receiptpocket/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.receiptpocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set ToolBar
+        val toolbar = findViewById<Toolbar>(R.id.login_toolbar)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+
     }
 }
